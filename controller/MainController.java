74da8ccddf91d24cdb4521e5c8347927e51ed76a
Kaique N <kaiquenascente@gmail.com>
2021-06-27 12:32:32 -0300

    passwordfield

---
+++ controller/MainController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController implements Initializable {
+
+    @FXML TextField textField;
+    @FXML PasswordField passwordField;
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {    
+    } 
+}
